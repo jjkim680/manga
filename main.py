783285af11b33fd4261ed51f42954e80d5aebcf2
Joseph Kim <jjkim573@gmail.com>
2026-04-21 13:16:50 -0700

    added index.html

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import time
 import random
 from cubari_5apps_client import fetch_cubari_data
 from weebcentral_client import fetch_slug_from_title, is_read
+import platform
+import json
 
 def read_manga():
     manga_titles = []
@@ -16,14 +18,23 @@ def read_manga():
 
     for manga_dict in manga_data.values():
         time.sleep(random.uniform(0.5, 3.5))
-        manga_titles.append(manga_dict.get("title"))
-        if is_read(manga_dict=manga_dict):
-            unread.append(0)
-        else:
-            unread.append(1)
+        manga_dict["caught_up"] = is_read(manga_dict=manga_dict)
 
-    print(manga_titles)
-    print(unread)
+    # test = [manga_dict.get("caught_up") for manga_dict in manga_data]
+    # print(test)
+
+    if platform.system() == 'Linux':
+        save_path = '/var/www/html/manga_data.json'
+        print(f"Linux detected. Saving to web server at: {save_path}")
+    else:
+        # os.path.join with os.getcwd() ensures it saves in the same folder it's run from
+        save_path = os.path.join(os.getcwd(), 'manga_data.json')
+        print(f"Not linux detected. Saving locally at: {save_path}")
+
+    # Saved into json file for webpage to use
+    with open(save_path, 'w', encoding='utf-8') as f:
+        json.dump(manga_data, f, ensure_ascii=False, indent=4)
+
 
 if __name__ == "__main__":
     read_manga()
